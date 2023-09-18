7bc47263c0bfb25e848a28dd1f1748c4d0a77bc4
Andrew Kane <andrew@ankane.org>
2023-09-18 16:25:51 -0700

    Updated variable name

--- test/pqxx_test.cpp
+++ test/pqxx_test.cpp
@@ -4,50 +4,50 @@
 #include <pqxx/pqxx>
 
 void setup(pqxx::connection &conn) {
-  pqxx::work txn{conn};
-  txn.exec0("CREATE EXTENSION IF NOT EXISTS vector");
-  txn.exec0("DROP TABLE IF EXISTS items");
-  txn.exec0("CREATE TABLE items (id serial PRIMARY KEY, embedding vector(3))");
-  txn.commit();
+  pqxx::work tx{conn};
+  tx.exec0("CREATE EXTENSION IF NOT EXISTS vector");
+  tx.exec0("DROP TABLE IF EXISTS items");
+  tx.exec0("CREATE TABLE items (id serial PRIMARY KEY, embedding vector(3))");
+  tx.commit();
 }
 
 void test_works(pqxx::connection &conn) {
-  pqxx::work txn{conn};
+  pqxx::work tx{conn};
   auto embedding = pgvector::Vector({1, 2, 3});
   assert(embedding.dimensions() == 3);
   float arr[] = {4, 5, 6};
   auto embedding2 = pgvector::Vector(arr, 3);
-  txn.exec_params("INSERT INTO items (embedding) VALUES ($1), ($2), ($3)",
+  tx.exec_params("INSERT INTO items (embedding) VALUES ($1), ($2), ($3)",
                   embedding, embedding2, std::nullopt);
 
-  pqxx::result res{txn.exec_params(
+  pqxx::result res{tx.exec_params(
       "SELECT embedding FROM items ORDER BY embedding <-> $1", embedding2)};
   assert(res.size() == 3);
   assert(res[0][0].as<pgvector::Vector>() == embedding2);
   assert(res[1][0].as<pgvector::Vector>() == embedding);
   assert(!res[2][0].as<std::optional<pgvector::Vector>>().has_value());
-  txn.commit();
+  tx.commit();
 }
 
 void test_stream(pqxx::connection &conn) {
-  pqxx::work txn{conn};
+  pqxx::work tx{conn};
   int count = 0;
-  for (auto [id, embedding] : txn.stream<int, pgvector::Vector>("SELECT id, embedding FROM items WHERE embedding IS NOT NULL")) {
+  for (auto [id, embedding] : tx.stream<int, pgvector::Vector>("SELECT id, embedding FROM items WHERE embedding IS NOT NULL")) {
     assert(embedding.dimensions() == 3);
     count++;
   }
   assert(count == 2);
-  txn.commit();
+  tx.commit();
 }
 
 void test_precision(pqxx::connection &conn) {
-  pqxx::work txn{conn};
+  pqxx::work tx{conn};
   auto embedding = pgvector::Vector({1.23456789, 0, 0});
-  txn.exec_params("INSERT INTO items (embedding) VALUES ($1)", embedding);
-  txn.exec0("SET extra_float_digits = 3");
-  pqxx::result res{txn.exec_params("SELECT embedding FROM items ORDER BY id DESC LIMIT 1")};
+  tx.exec_params("INSERT INTO items (embedding) VALUES ($1)", embedding);
+  tx.exec0("SET extra_float_digits = 3");
+  pqxx::result res{tx.exec_params("SELECT embedding FROM items ORDER BY id DESC LIMIT 1")};
   assert(res[0][0].as<pgvector::Vector>() == embedding);
-  txn.commit();
+  tx.commit();
 }
 
 int main() {
