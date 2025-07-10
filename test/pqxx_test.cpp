5d6a52fcbb12fd2f3a5f5b6b1d966ce22c62f8c7
Andrew Kane <andrew@ankane.org>
2025-07-10 09:39:37 -0700

    Improved tests

--- test/pqxx_test.cpp
+++ test/pqxx_test.cpp
@@ -34,6 +34,9 @@ void test_vector(pqxx::connection &conn) {
     assert(res[0][0].as<pgvector::Vector>() == embedding2);
     assert(res[1][0].as<pgvector::Vector>() == embedding);
     assert(!res[2][0].as<std::optional<pgvector::Vector>>().has_value());
+
+    assert(pqxx::to_string(embedding) == "[1,2,3]");
+    assert(pqxx::from_string<pgvector::Vector>("[1,2,3]") == embedding);
 }
 
 void test_halfvec(pqxx::connection &conn) {
@@ -51,6 +54,9 @@ void test_halfvec(pqxx::connection &conn) {
     assert(res[0][0].as<pgvector::HalfVector>() == embedding2);
     assert(res[1][0].as<pgvector::HalfVector>() == embedding);
     assert(!res[2][0].as<std::optional<pgvector::HalfVector>>().has_value());
+
+    assert(pqxx::to_string(embedding) == "[1,2,3]");
+    assert(pqxx::from_string<pgvector::HalfVector>("[1,2,3]") == embedding);
 }
 
 void test_bit(pqxx::connection &conn) {
@@ -81,6 +87,10 @@ void test_sparsevec(pqxx::connection &conn) {
     assert(res[0][0].as<std::string>() == "{1:4,2:5,3:6}/3");
     assert(res[1][0].as<std::string>() == "{1:1,2:2,3:3}/3");
     assert(!res[2][0].as<std::optional<std::string>>().has_value());
+
+    assert(pqxx::to_string(embedding) == "{1:1,2:2,3:3}/3");
+    // TODO add
+    // assert(pqxx::from_string<pgvector::SparseVector>("{1:1,2:2,3:3}/3") == embedding);
 }
 
 void test_sparsevec_nnz(pqxx::connection &conn) {
