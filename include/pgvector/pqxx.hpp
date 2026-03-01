9dacbdfae1f187ebca3ec2a0b23ceb283454f3ba
Andrew Kane <andrew@ankane.org>
2026-03-01 15:43:40 -0800

    Simplified code [skip ci]

--- include/pgvector/pqxx.hpp
+++ include/pgvector/pqxx.hpp
@@ -45,7 +45,7 @@ template <> struct string_traits<pgvector::Vector> {
     }
 
     static std::string_view to_buf(std::span<char> buf, const pgvector::Vector& value, ctx c = {}) {
-        auto values = static_cast<std::span<const float>>(value);
+        std::span<const float> values{value};
 
         // important! size_buffer cannot throw an exception on overflow
         // so perform this check before writing any data
@@ -69,7 +69,7 @@ template <> struct string_traits<pgvector::Vector> {
     }
 
     static size_t size_buffer(const pgvector::Vector& value) noexcept {
-        auto values = static_cast<std::span<const float>>(value);
+        std::span<const float> values{value};
 
         // cannot throw an exception here on overflow
         // so throw in into_buf
@@ -109,7 +109,7 @@ template <> struct string_traits<pgvector::HalfVector> {
     }
 
     static std::string_view to_buf(std::span<char> buf, const pgvector::HalfVector& value, ctx c = {}) {
-        auto values = static_cast<std::span<const float>>(value);
+        std::span<const float> values{value};
 
         // important! size_buffer cannot throw an exception on overflow
         // so perform this check before writing any data
@@ -133,7 +133,7 @@ template <> struct string_traits<pgvector::HalfVector> {
     }
 
     static size_t size_buffer(const pgvector::HalfVector& value) noexcept {
-        auto values = static_cast<std::span<const float>>(value);
+        std::span<const float> values{value};
 
         // cannot throw an exception here on overflow
         // so throw in into_buf
